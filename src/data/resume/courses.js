7dcf5cf6110f5fa405fb353d8f85a03a185e323d
Maithri <maithri.sridhar@gmail.com>
2023-08-26 18:18:20 +0530

    Update courses.js

--- src/data/resume/courses.js
+++ src/data/resume/courses.js
@@ -16,7 +16,21 @@ const courses = [
     number: 'explorer-camp',
     link: 'https://www.science.edu.sg/',
     university: 'Science Centre',
+	
   },
+  { 
+    title: 'AoPS Introduction to Number Theory,
+	number: 'number theory',
+	link: 'https://artofproblemsolving.com/school/course/intro-numbertheory'
+	university: 'AoPS,
+  },
+  { 
+    title: 'AoPS Introduction to Geometry,
+	number: 'geometry',
+	link: 'https://artofproblemsolving.com/school/course/intro-geometry'
+	university: 'AoPS,
+  },
+  { 
 ];
 
 export default courses;
