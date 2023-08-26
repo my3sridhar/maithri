74c6c22840c055e90ab85a90b2f209facb2b5a01
Sridhar K. N. Rao <srao@linuxfoundation.org>
2023-08-26 22:21:02 +0530

    Bugs Round 3

--- src/data/resume/courses.js
+++ src/data/resume/courses.js
@@ -17,13 +17,13 @@ const courses = [
     link: 'https://www.science.edu.sg/',
     university: 'Science Centre',
   },
-  { 
+  {
     title: 'AoPS Introduction to Number Theory',
     number: 'number theory',
     link: 'https://artofproblemsolving.com/school/course/intro-numbertheory',
     university: 'AoPS',
   },
-  { 
+  {
     title: 'AoPS Introduction to Geometry',
     number: 'geometry',
     link: 'https://artofproblemsolving.com/school/course/intro-geometry',
