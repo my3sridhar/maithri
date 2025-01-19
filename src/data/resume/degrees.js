c108e6858d0425c77aa63c0ab0f2612a23ff4b72
Maithri <maithri.sridhar@gmail.com>
2025-01-19 19:13:11 +0530

    Revert "Update degrees.js"
    This reverts commit 78fb259606a1af422e3688ad2cb07254ad691303.

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -1,11 +1,5 @@
 const degrees = [
   {
-    school: 'NPS RNR, Bangalore, India',
-    degree: 'From Grade 11 - 12',
-    link: 'https://www.npsrnr.com/',
-    year: 2025,
-  },
-  {
     school: 'NAFL, Basaveshwarnagar, Bangalore, India',
     degree: 'From Grade 6 - Current',
     link: 'https://nafl.in',
