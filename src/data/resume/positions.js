3a98bc1b496493bd526c5d06e1ef019d4461715c
Maithri <maithri.sridhar@gmail.com>
2025-01-19 19:56:24 +0530

    Update positions.js

--- src/data/resume/positions.js
+++ src/data/resume/positions.js
@@ -2,7 +2,7 @@ const positions = [
   {
     company: 'Indo French Hub',
     position: '3rd Place in the world',
-    link: 'https://indofrenchhub.com/concours-2024-lettre-au-pere-noel/',
+    link: 'https://indofrenchhub.com/concours-2024-lettre-au-pere-noel/ ',
     daterange: 'December 2024 - January 2025',
     points: [
       'Came Third in an Internation French Writing competition, among other professionals, adults and French teachers. Also won a cash prize of 7000 rupees',
@@ -11,7 +11,7 @@ const positions = [
   {
     company: 'LCM Piano',
     position: 'Grade-4',
-    link: 'https://lcme.uwl.ac.uk/exams/piano/piano',
+    link: 'https://lcme.uwl.ac.uk/our-exams/piano/piano/ ',
     daterange: 'November 2023 - December 2024',
     points: [
       'Scored 85 out of 100',
@@ -20,7 +20,7 @@ const positions = [
   {
     company: 'LCM Piano',
     position: 'Grade-3',
-    link: 'https://lcme.uwl.ac.uk/exams/piano/piano',
+    link: 'https://lcme.uwl.ac.uk/our-exams/piano/piano/ ',
     daterange: 'September 2022 - August 2023',
     points: [
       'Scored 93 out of 100',
@@ -29,7 +29,7 @@ const positions = [
   {
     company: 'LCM Piano',
     position: 'Grade-2',
-    link: 'https://lcme.uwl.ac.uk/exams/piano/piano',
+    link: 'https://lcme.uwl.ac.uk/our-exams/piano/piano/ ',
     daterange: 'October 2021 - August 2022',
     points: [
       'Scored 100 out of 100',
@@ -38,7 +38,7 @@ const positions = [
   {
     company: 'LCM Piano',
     position: 'Grade-1',
-    link: 'https://lcme.uwl.ac.uk/exams/piano/piano',
+    link: 'https://lcme.uwl.ac.uk/our-exams/piano/piano/ ',
     daterange: 'January 2020 - August 2021',
     points: [
       'Scored 100 out of 100',
@@ -47,7 +47,7 @@ const positions = [
   {
     company: 'Delf Junior French',
     position: 'B1',
-    link: 'http://www.delfdalf.fr/delf-junior-version.html',
+    link: 'http://www.delfdalf.fr/delf-junior-version.html ',
     daterange: 'May 2022 - April 2023',
     points: [
       'Merit - 65%',
@@ -56,7 +56,7 @@ const positions = [
   {
     company: 'Delf Junior French',
     position: 'A2',
-    link: 'http://www.delfdalf.fr/delf-junior-version.html',
+    link: 'http://www.delfdalf.fr/delf-junior-version.html ',
     daterange: 'March 2021 - April 2022',
     points: [
       'Distinction - 91%',
@@ -65,7 +65,7 @@ const positions = [
   {
     company: 'Delf Junior French',
     position: 'A1',
-    link: 'http://www.delfdalf.fr/delf-junior-version.html',
+    link: 'http://www.delfdalf.fr/delf-junior-version.html ',
     daterange: 'January 2020 - December 2021',
     points: [
       'Distiction - 96%',
@@ -74,7 +74,7 @@ const positions = [
   {
     company: 'Kumon',
     position: 'H-Level',
-    link: 'https://www.kumon.com',
+    link: 'https://www.kumon.com ',
     daterange: 'September 2016 - 2020',
     points: [
       'Was an Honor Roll, Advanced Student - 2016-17, 2017-18, 2019-20',
@@ -83,7 +83,7 @@ const positions = [
   {
     company: 'Presidency Science Exhibition',
     position: '2nd Prize',
-    link: 'https://presidencynlo.org',
+    link: 'https://presidencynlo.org ',
     daterange: 'February 2019',
     points: [
       'Won prize for Electrical circuits',
@@ -92,7 +92,7 @@ const positions = [
   {
     company: 'International Olympiad of Science',
     position: 'Silver Medal',
-    link: 'https://www.theinternationalolympiad.com/',
+    link: 'https://www.theinternationalolympiad.com/ ',
     daterange: 'July 2015',
     points: [
       'All India Silver Medal, School Topper.',
